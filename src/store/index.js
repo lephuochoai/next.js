0d19aeb095d4b94c9997acaa79adfb4acbbbd067
hoailp <hoailp@bap.jp>
2021-06-24 16:23:34 +0700

    Fix show redux devTool on production

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import { rootReducer } from './slices'
 
 const store = configureStore({
-  reducer: rootReducer()
+  reducer: rootReducer(),
+  devTools: process.env.NODE_ENV === 'development' ? true : false
 })
 
 export default store
