2ed1129e162828f364d1ad011d1c7643fb511b43
hoailp <hoailp@bap.jp>
2021-06-25 11:54:27 +0700

    Fix search page

--- src/page-components/search/Search.js
+++ src/page-components/search/Search.js
@@ -22,17 +22,16 @@ export const Search = (props) => {
 
   useEffect(() => {
     setQ(router.query?.q)
+    onSearch()
   }, [router.query])
 
-  const onSearch = (e) => {
-    e.preventDefault()
+  const onSearch = () => {
     setSearching(true)
-    router.push(`/search?q=${q}`)
     if (q) {
       if (listRecentSearch.length) {
-        setListRecentSearch([...listRecentSearch, q])
+        setListRecentSearch([q, ...listRecentSearch.slice(0, 10)])
         localStorage.setItem('recent-search',
-          JSON.stringify([...listRecentSearch, q]))
+          JSON.stringify([q, ...listRecentSearch.slice(0, 10)]))
       } else {
         setListRecentSearch([q])
         localStorage.setItem('recent-search', JSON.stringify([q]))
@@ -47,8 +46,14 @@ export const Search = (props) => {
     }, 500)
   }
 
+  const onSubmitFormSearch = (e) => {
+    e?.preventDefault()
+    router.push(`/search?q=${q}`)
+    onSearch()
+  }
+
   const clearRecentSearch = (index) => {
-    let list = [...listRecentSearch]
+    let list = [...listRecentSearch.slice(0, 10)]
     list.splice(index, 1)
     localStorage.setItem('recent-search', JSON.stringify(list))
     setListRecentSearch(list)
@@ -80,36 +85,38 @@ export const Search = (props) => {
   return (
     <PageContainer className={cx(props.className)}>
       <HtmlMeta title="Search" />
-      <div className="container row">
-        <div className="col-lg-8">
-          <form onSubmit={onSearch} className="form-search d-flex">
-            <Input
-              value={q}
-              onChange={(e) => setQ(e.target.value)}
-              className="me-3"
-              disabled={searching}
-            />
-            <Button
-              htmlType="submit"
-              disabled={searching}
-              icon={<span className="anticon"><BsSearch /></span>}
-              type="primary">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8">
+            <form onSubmit={onSubmitFormSearch} className="form-search d-flex">
+              <Input
+                value={q}
+                onChange={(e) => setQ(e.target.value)}
+                className="me-3"
+                disabled={searching}
+              />
+              <Button
+                htmlType="submit"
+                disabled={searching}
+                icon={<span className="anticon"><BsSearch /></span>}
+                type="primary">
               Search
-            </Button>
-          </form>
-          <div className="result-search mt-4">
-            <center>
-              {searching ? <LoadingOutlined /> : (
-                <b>We couldn't find anything</b>
-              )}
-            </center>
+              </Button>
+            </form>
+            <div className="result-search mt-4">
+              <center>
+                {searching ? <LoadingOutlined /> : (
+                  <b>We couldn't find anything</b>
+                )}
+              </center>
+            </div>
           </div>
-        </div>
-        <div className="col-lg-4">
-          <h5 className="lh-lg">Recent search</h5>
-          <ul>
-            {renderRecentSearch}
-          </ul>
+          <div className="col-lg-4">
+            <h5 className="lh-lg">Recent search</h5>
+            <ul>
+              {renderRecentSearch}
+            </ul>
+          </div>
         </div>
       </div>
     </PageContainer>
